21fdfea8f6f80e25d6090bb445d7c8dcbe0f2c5c
AugustoMizu <augusto10barros@gmail.com>
2025-03-08 18:51:43 -0300

    o mesmo

--- includes/valida_login.php
+++ includes/valida_login.php
@@ -2,15 +2,15 @@
 session_start();
 
 // Verifica se o usuário está logado
-if (!isset($_SESSION['usuario'])) {
+/*if (!isset($_SESSION['usuario'])) {
     // Redireciona para a página de login se não estiver logado
     header("Location: pagina_inicial.php");
     exit();
-}
+}*/
 
 // Verifica o tipo de usuário
-$tipoUsuario = $_SESSION['usuario']['tipo'];
-
+$tipoUsuario = $_SESSION['usuario']['CLIENTE'];
+// $tipoUsuario = $_SESSION['usuario']['tipo'];
 
 // Função para verificar permissões
 function verificarPermissao($tipoPermitido)
@@ -21,7 +21,7 @@ function verificarPermissao($tipoPermitido)
         return;
     } else if ($tipoUsuario !== $tipoPermitido) {
         // Redireciona para uma página inicial se o usuário não tiver permissão EMPRESA ou CLIENTE
-        header("Location: pagina_inicial.php");
+        header("Location: ../paginas/pagina_inicial.php");
         exit();
     }
 }
