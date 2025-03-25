c015ba29e597b4da629822b09121e4f4565b0d38
AugustoMizu <augusto10barros@gmail.com>
2025-03-25 01:20:22 -0300

    nova main padrão

--- banco_cadastro_login.sql
+++ banco_cadastro_login.sql
@@ -7,13 +7,8 @@ CREATE TABLE clientes (
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL, -- A senha será armazenada criptografada
     telefone VARCHAR(20),
-<<<<<<< Updated upstream
-    tipo DEFAULT 'cliente',
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-=======
     tipo VARCHAR(20) DEFAULT 'cliente', 
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP --  ^ a mesma coisa pq não sei se isso vai ser viável 
->>>>>>> Stashed changes
 );
 
 CREATE TABLE empresas (
@@ -23,11 +18,7 @@ CREATE TABLE empresas (
     senha VARCHAR(255) NOT NULL, -- A senha será armazenada criptografada
     telefone VARCHAR(20),
     cnpj VARCHAR(20) UNIQUE NOT NULL,
-<<<<<<< Updated upstream
-    tipo DEFAULT 'empresa',
-=======
     tipo VARCHAR(20) DEFAULT 'empresa',
->>>>>>> Stashed changes
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
